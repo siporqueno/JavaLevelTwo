12d686a77cedd43ce47e5524fcabd6f68585ba46
siporqueno <strike1000001@gmail.com>
2019-06-18 02:34:43 +0300

    First attempt to tackle home work

--- src/Lesson_2/Main.java
+++ src/Lesson_2/Main.java
@@ -2,11 +2,13 @@ package Lesson_2;
 
 public class Main {
     static String[][] testArrayOne = {
-            {"1", "1", "1", "1",},
-            {"1", "1", "1", "1",},
-            {"1", "1", "1", "1",},
+            {"1", "1", "1", "1"},
+            {"1", "???", "1", "1",},
+            {"1", "1", "1"},
             {"1", "1", "1", "1",}
     };
+
+    int a = +1;
 
     public static void main(String[] args) {
         try {
@@ -14,31 +16,62 @@ public class Main {
         } catch (MyArraySizeException eSize) {
             eSize.printStackTrace();
         } catch (MyArrayDataException eData) {
+            System.out.printf("В ячейке %d %d нет числа\n", eData.getRowNo(), eData.getColNo());
             eData.printStackTrace();
         }
 
     }
 
-    int sumOfStrArr(String[][] arrTwoDim) {
+    int sumOfStrArr(String[][] arrTwoDim) throws MyArraySizeException, MyArrayDataException {
         int sum = 0;
         if (arrTwoDim.length != 4) throw new MyArraySizeException();
 
-        for (String[] arrOneDim : arrTwoDim) {
-            if (arrOneDim.length != 4) throw new MyArraySizeException();
-            for (String strElem : arrOneDim) {
-//                if (Integer.parseInt(strElem) instanceof int)
-                sum += Integer.parseInt(strElem);
+        for (int i = 0; i < arrTwoDim.length; i++) {
+            if (arrTwoDim[i].length != 4) throw new MyArraySizeException();
+            for (int j = 0; j < arrTwoDim[i].length; j++) {
+//                if (isParseableToInt(arrTwoDim[i][j])) throw new MyArrayDataException();
+                try {
+                    sum += Integer.parseInt(arrTwoDim[i][j]);
+
+                } catch (NumberFormatException e) {
+                    System.out.println("В строке НЕ число!");
+                    throw new MyArrayDataException("В этой строке нет числа!!!", i, j);
+                }
+                }
             }
+
+            return sum;
         }
 
-        return sum;
+        private boolean isParseableToInt (String s){
+            s = s.trim();
+            if (s.length() == 0) return false;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c < '0' || c > '9') return false;
+            }
+            return true;
+        }
     }
-}
 
-class MyArraySizeException extends ArrayIndexOutOfBoundsException {
+    class MyArraySizeException extends ArrayIndexOutOfBoundsException {
 
-}
+    }
 
-class MyArrayDataException extends NumberFormatException {
+    class MyArrayDataException extends NumberFormatException {
+        private int rowNo, colNo;
 
-}
+        public MyArrayDataException(String s, int rowNo, int colNo) {
+            super(s);
+            this.rowNo = rowNo;
+            this.colNo = colNo;
+        }
+
+        public int getRowNo() {
+            return rowNo;
+        }
+
+        public int getColNo() {
+            return colNo;
+        }
+    }
